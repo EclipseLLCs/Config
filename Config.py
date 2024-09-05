ecaac75740175b062f52ca219dd9a5593769979e
EclipseLLCs <frostbytemailing@gmail.com>
2024-09-05 18:22:41 +0300

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,23 @@
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------
+# EN: 
+#     - Do not touch or modify the code below. If there is an error, please contact the owner, but under no circumstances should you touch the code.
+#     - Do not resell this tool, do not credit it to yours.
+# FR: 
+#     - Ne pas toucher ni modifier le code ci-dessous. En cas d'erreur, veuillez contacter le propriétaire, mais en aucun cas vous ne devez toucher au code.
+#     - Ne revendez pas ce tool, ne le créditez pas au vôtre.
+
+name_tool = "Eclipsing"
+type_tool = "Multi-Tools"
+version_tool = "0.1"
+coding_tool = "Python 3"
+language_tool = "EN"
+creator = "Eclipse"
+platform = "Windows 10/11 & Linux"
+discord_server = "discord.gg/Eclipse LLC"
+website = "eclipsing.sellauth.com"
+github_tool = "github.com"
+telegram = "t.me"
+url_config = "https://raw.githubusercontent.com/EclipseLLCs/Config/main/Config.py"
+url_downloads = "discord.gg/EclipseLLC"
+license = "discord.gg/EclipseLLC"
+copyright = "CopyRight (c) Eclipse LLC 'LICENSE'"
